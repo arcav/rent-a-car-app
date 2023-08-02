9d566a7de2375ee9ba2595cd587d6e970aa37693
arcav <armandojcastro84@gmail.com>
2023-08-01 20:46:00 -0400

    feat(utils):function to get dynamic car properties

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,4 @@
-
+import { CarProps } from "@/types";
 
 export async function fetchCars() {
   const headers = {
@@ -27,4 +27,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, year, model } = car;
+  url.searchParams.append("make", make);
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
 };
