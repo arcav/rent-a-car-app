d3f2f2778503ccc630e13b4ebdc775c9b2bd9d7a
arcav <armandojcastro84@gmail.com>
2023-08-01 22:56:50 -0400

    style(CarDetails):angle car

--- src/components/CarDetails.tsx
+++ src/components/CarDetails.tsx
@@ -57,17 +57,17 @@ const CarDetails = ({ isOpen, closeModal, car }: CardDetailsProps) => {
                   <div className="flex-1 flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src={generateCarImageUrl(car, "angle")}
+                        src={generateCarImageUrl(car)}
                         alt="car model"
                         fill
                         priority
                         className="object-contain"
                       />
                     </div>
-                    <div className="flex gap-3">
+                    <div className="flex gap-3 mt-2">
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "29")}
                           alt="car model"
                           fill
                           priority
@@ -76,16 +76,16 @@ const CarDetails = ({ isOpen, closeModal, car }: CardDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "33")}
                           alt="car model"
                           fill
                           priority
-                          className="object-contain"
+                          className="object-contain mt-4"
                         />
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "13")}
                           alt="car model"
                           fill
                           priority
